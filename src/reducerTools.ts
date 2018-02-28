251b1fbcb61eece648ee5552c96ebddad9d4b160
Paul Nilsson <paul.nilsson@smokeball.com>
2018-02-28 13:00:27 +1100

    added makeActionPartialReducer, makeOptionalPropertyReducer, and makeToggleReducer

--- src/reducerTools.ts
+++ src/reducerTools.ts
@@ -8,21 +8,33 @@ export const autoReducerFactory = <AT>(actionTypes : AT) => <AD extends { [T in 
   const reducers = {} as { [T in keyof AT] : {
     actionDataType : AD[T],
     makeActionReducer : (defaultState : AD[T]) => (state : AD[T] | undefined, action : Action) => AD[T],
+    makeActionPartialReducer : (defaultState : Partial<AD[T]>) => (state : Partial<AD[T]> | undefined, action : Action) => Partial<AD[T]>,
     makePropertyReducer : <P extends keyof AD[T]>(property : P, defaultState : AD[T][P]) => (state : AD[T][P] | undefined, action : Action) => AD[T][P],
+    makeOptionalPropertyReducer : <P extends keyof AD[T]>(property : P) => (state : AD[T][P] | undefined, action : Action) => AD[T][P] | undefined,
+    makeToggleReducer : (defaultState? : boolean ) => (state : boolean | undefined, action : Action) => boolean,
   } };
 
   for (const type in actionTypes) {
     reducers[type] = {
       actionDataType: {},
-      makeActionReducer: (defaultState) => (state, action) => guards[type](action) ? action.data : state || defaultState,
-      makePropertyReducer: (field, defaultState) => (state, action) => guards[type](action) ? action.data[field] : state || defaultState,
+      makeActionReducer: (defaultState) => (state, action) => guards[type](action) ? action.data : (state !== undefined) ? state : defaultState,
+      makeActionPartialReducer: (defaultState) => (state, action) => guards[type](action) ? action.data : (state !== undefined) ? state : defaultState,
+      makePropertyReducer: (field, defaultState) => (state, action) => guards[type](action) ? action.data[field] : (state !== undefined) ? state : defaultState,
+      makeOptionalPropertyReducer: (field) => (state, action) => guards[type](action) ? action.data[field] : state,
+      makeToggleReducer: (defaultState = false) => (state, action) => {
+        const currentState = !!(state !== undefined ? state : defaultState);
+
+        return guards[type](action) ? !currentState : currentState;
+      },
     };
   }
 
   return reducers;
 };
 
-export type TReducers<S> = { [K in keyof S] : (state : S[K] | undefined, action : Action) => S[K] };
+export type TTightReducer<S> = (state : S | undefined, action : Action) => S;
+export type TReducers<S> = { [K in keyof S] : TTightReducer<S[K]> };
 
 // tslint:disable-next-line no-unnecessary-callback-wrapper (it's enforcing type safety)
 export const tightCombineReducers = <State>(reducers : TReducers<State>) => combineReducers<State>(reducers); // not needed if using redux 4+ as TS support improved
+
--- src/test/reducerTools.test.ts
+++ src/test/reducerTools.test.ts
@@ -5,12 +5,14 @@ enum ActionTypes {
   Foo,
   Bar,
   Baz,
+  Tog,
 }
 
 type TActionDataShape = {
   Foo : { id : string };
   Bar : { id : number };
   Baz : { first : boolean, second : number, third : string };
+  Tog : { [index : string] : never};
 };
 
 const autoCreators = autoCreatorFactory(ActionTypes)<TActionDataShape>();
@@ -47,6 +49,37 @@ test('autoreducer ignores irrelevant actions in action mode', () => {
   expect(state).toEqual({ id : 123 });
 });
 
+test('autoreducer will init state in partial action mode', () => {
+  let state : Partial<typeof autoReducers.Bar.actionDataType>;
+  const reducer = autoReducers.Bar.makeActionPartialReducer({});
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual({ });
+});
+
+test('autoreducer processes relevant actions in partial action mode', () => {
+  let state : Partial<typeof autoReducers.Bar.actionDataType>;
+  const reducer = autoReducers.Bar.makeActionPartialReducer({});
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual({ });
+  state = reducer(state, autoCreators.Bar({id : 1213}));
+  expect(state).toEqual({ id : 1213 });
+});
+
+test('autoreducer ignores irrelevant actions in partial action mode', () => {
+  let state : Partial<typeof autoReducers.Bar.actionDataType>;
+  const reducer = autoReducers.Bar.makeActionPartialReducer({});
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual({ });
+  state = reducer(state, autoCreators.Foo({id : '1213'}));
+  expect(state).toEqual({ });
+});
+
 test('autoreducer will init state in property mode', () => {
   let state : typeof autoReducers.Bar.actionDataType.id;
   const reducer = autoReducers.Bar.makePropertyReducer('id', 123);
@@ -56,7 +89,7 @@ test('autoreducer will init state in property mode', () => {
   expect(state).toEqual(123);
 });
 
-test('autoreducer processes relevant actions in action mode', () => {
+test('autoreducer processes relevant actions in property mode', () => {
   let state : typeof autoReducers.Bar.actionDataType.id;
   const reducer = autoReducers.Bar.makePropertyReducer('id', 123);
 
@@ -67,7 +100,7 @@ test('autoreducer processes relevant actions in action mode', () => {
   expect(state).toEqual(1213);
 });
 
-test('autoreducer ignores irrelevant actions in action mode', () => {
+test('autoreducer ignores irrelevant actions in property mode', () => {
   let state : typeof autoReducers.Bar.actionDataType.id;
   const reducer = autoReducers.Bar.makePropertyReducer('id', 123);
 
@@ -77,3 +110,83 @@ test('autoreducer ignores irrelevant actions in action mode', () => {
   state = reducer(state, autoCreators.Foo({id : '1213'}));
   expect(state).toEqual(123);
 });
+
+test('autoreducer will init undefined state in optional property mode', () => {
+  let state : typeof autoReducers.Bar.actionDataType.id | undefined;
+  const reducer = autoReducers.Bar.makeOptionalPropertyReducer('id');
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toBeUndefined();
+});
+
+test('autoreducer processes relevant actions in optional property mode', () => {
+  let state : typeof autoReducers.Bar.actionDataType.id | undefined;
+  const reducer = autoReducers.Bar.makeOptionalPropertyReducer('id');
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toBeUndefined();
+  state = reducer(state, autoCreators.Bar({id : 1213}));
+  expect(state).toEqual(1213);
+});
+
+test('autoreducer ignores irrelevant actions in optional property mode', () => {
+  let state : typeof autoReducers.Bar.actionDataType.id | undefined;
+  const reducer = autoReducers.Bar.makeOptionalPropertyReducer('id');
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toBeUndefined();
+  state = reducer(state, autoCreators.Foo({id : '1213'}));
+  expect(state).toBeUndefined();
+});
+
+test('autoreducer will init false state in toggle mode without default', () => {
+  let state : boolean;
+  const reducer = autoReducers.Tog.makeToggleReducer();
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual(false);
+});
+
+test('autoreducer can init to true state in toggle mode with default', () => {
+  let state : boolean;
+  const reducer = autoReducers.Tog.makeToggleReducer(true);
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual(true);
+});
+
+test('autoreducer processes relevant actions in toggle mode', () => {
+  let state : boolean;
+  const reducer = autoReducers.Tog.makeToggleReducer();
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual(false);
+  state = reducer(state, autoCreators.Tog({}));
+  expect(state).toEqual(true);
+  state = reducer(state, autoCreators.Tog({}));
+  expect(state).toEqual(false);
+  state = reducer(state, autoCreators.Tog({}));
+  expect(state).toEqual(true);
+  state = reducer(state, autoCreators.Tog({}));
+  expect(state).toEqual(false);
+});
+
+test('autoreducer ignores irrelevant actions in toggle mode', () => {
+  let state : boolean;
+  const reducer = autoReducers.Tog.makeToggleReducer();
+
+  state = reducer(undefined, { type : '' }); // init state with blank action
+
+  expect(state).toEqual(false);
+  state = reducer(state, autoCreators.Bar({ id : 123 }));
+  expect(state).toEqual(false);
+  state = reducer(state, autoCreators.Bar({ id : 123 }));
+  expect(state).toEqual(false);
+});
+
